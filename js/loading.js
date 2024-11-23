65728f5e4a08b24509cf2be6f8afbf219755abb9
David Renne <davidrenne@gmail.com>
2024-11-23 00:25:43 -0500

    New stuff

--- js/loading.js
+++ js/loading.js
@@ -1,27 +1,29 @@
-    var dots = ["."];
-    var totalSeconds = 0;
-    var dotInterval = 0;
-    setTimeout(function() {
-      clearInterval(dotInterval)
-    }, 60 * 1000);
-    setTimeout(function() {
-      if (document.getElementById("loadingText")) {
-        console.error(document.getElementById("loadingText"))
-        document.getElementById("loadingText").innerHTML = "Loading ";
-        dotInterval = setInterval(function() {
-          if (document.getElementById("dotdotdot")) {
-            if (totalSeconds < 10) {
-              document.getElementById("dotdotdot").innerHTML = dots.join(" ");
-              dots.push(".");
-              if (dots.length === 4) {
-                dots = ["."];
-              }
-              totalSeconds++;
-            } else {
-              document.getElementById("loadingText").innerHTML = "";
-              document.getElementById("dotdotdot").innerHTML = "Slow client connection, please wait...";
-            }
+var dots = ["."];
+var totalSeconds = 0;
+var dotInterval = 0;
+setTimeout(function () {
+  clearInterval(dotInterval);
+}, 60 * 1000);
+
+setTimeout(function () {
+  if (document.getElementById("loadingText")) {
+    console.error(document.getElementById("loadingText"));
+    document.getElementById("loadingText").innerHTML = "Loading ";
+    dotInterval = setInterval(function () {
+      if (document.getElementById("dotdotdot")) {
+        if (totalSeconds < 10) {
+          document.getElementById("dotdotdot").innerHTML = dots.join(" ");
+          dots.push(".");
+          if (dots.length === 4) {
+            dots = ["."];
           }
-        }, 1000);
+          totalSeconds++;
+        } else {
+          document.getElementById("loadingText").innerHTML = "";
+          document.getElementById("dotdotdot").innerHTML =
+            "Slow client connection, please wait...";
+        }
       }
-    }, 3000);
+    }, 1000);
+  }
+}, 3000);
